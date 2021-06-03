a2ce580ea444771aca7a9217f7f11590bd571fe6
washua2 <714879247@qq.com>
2021-06-04 01:42:19 +0800

    保存项目格式

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,26 @@
     <router-view/>
   </div>
 </template>
+<script>
+  export default {
+    name:"app",
+    data(){
+      return{
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+      }
+    },
+    components:{
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
   }
+
+
+</script>
+
+<style lang="scss">
+@import "assets/css/base.css";
+#app {
+  text-align:center;
 }
+
 </style>
